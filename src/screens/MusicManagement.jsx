5d4c248e43165892c3920f27876ab9086e39919e
hungvu <hungns126@gmail.com>
2025-07-08 21:35:20 +0700

    WIP

--- src/screens/MusicManagement.jsx
+++ src/screens/MusicManagement.jsx
@@ -33,7 +33,7 @@ import {
   UserOutlined,
   PictureOutlined,
 } from "@ant-design/icons";
-import { removeVNTones } from "./utils";
+import { removeVNTones } from "../utils";
 
 const { Title, Text } = Typography;
 const { Dragger } = Upload;
@@ -529,8 +529,8 @@ function MusicManager() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <div className="max-w-7xl mx-auto">
+    <div className="min-h-screen bg-gray-50 p-6 flex justify-center">
+      <div className="w-full max-w-7xl">
         {/* Header */}
         <div className="mb-8">
           <Title level={2} className="text-center text-gray-800 mb-2">
