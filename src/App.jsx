5268b5260c61deec4fcb24176b227750c82eedde
hungvu <hungns126@gmail.com>
2025-07-08 23:23:25 +0700

    change new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import MusicManagement from "./screens/MusicManagement";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/manage" element={<MusicManagement />} />
+      </Routes>
+    </div>
+  );
 }
